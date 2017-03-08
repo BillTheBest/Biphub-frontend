92c6be30527d18e0011f3f86e5da8657e64f62c6
Jason Shin <visualbbasic@gmail.com>
2017-03-08 22:16:42 +1100

    feat: explore panel

---
+++ client/js/components/API/API.vue
@@ -0,0 +1,21 @@
+<style scoped lang="scss" src="./API.scss"></style>
+
+<template>
+  <div class="component">
+    <div class="api-icon">
+      <img v-bind:src="src">
+    </div>
+    <div class="api-label">
+      {{ label }}
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+    	src: { type:String, default: 'N/A' },
+    	label: { type: String, default: 'N/A' },
+    }
+  }
+</script>
--- client/js/components/ExplorePanel/ExplorePanel.vue
+++ client/js/components/ExplorePanel/ExplorePanel.vue
@@ -5,10 +5,39 @@
     <div class="component-pad">
       <SearchBar></SearchBar>
     </div>
+
+    <div class="component-pad api-list">
+      <API label="Discord"
+           src="static/icon-discord.png"
+      ></API>
+      <API label="Dota 2"
+           src="static/icon-dota2.png"
+      ></API>
+      <API label="Gmail"
+           src="static/icon-gmail.png"
+      ></API>
+      <API label="Hearthstone"
+           src="static/icon-hearthstone.png"
+      ></API>
+      <API label="Minecraft"
+           src="static/icon-minecraft.png"
+      ></API>
+      <API label="Overwatch"
+           src="static/icon-overwatch.png"
+      ></API>
+      <API label="Team Fortress2"
+           src="static/icon-tf2.png"
+      ></API>
+      <API label="WoW"
+           src="static/icon-wow.png"
+      ></API>
+
+    </div>
   </div>
 </template>
 
 <script>
+  import API from 'components/API'
   import Icon from 'components/Icon'
   import SearchBar from 'components/SearchBar'
 
@@ -16,6 +45,7 @@
     components: {
     	Icon,
       SearchBar,
+      API,
     }
   }
 </script>
